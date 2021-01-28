f883f8fe31cb1d41e4bcd2328eb3feda6b53c9ed
itzEric02 <49700269+itzEric02@users.noreply.github.com>
2021-01-28 13:36:46 +0100

    Update task-checker.sh

--- Crossfire Scripts/task-checker.sh
+++ Crossfire Scripts/task-checker.sh
@@ -2,6 +2,22 @@
 
 OPA=$1
 
+if ! type "jq" > /dev/null; then
+    echo please install jq
+fi
+
+if [ "$#" == 0 ]
+ then
+ printf "\n\e[31mERROR: Please add your operator address [crocncl1...] to the end of the command\e[0m\n"
+ exit 0
+fi
+
+if expr length "$OPA" != 46 > nul
+ then
+ printf "\x1b[31mERROR: Your operator address has not the correct length\x1b[0m\n"
+ exit 1
+fi
+
 OUTPUT=$(curl -sSL https://chain.crypto.com/explorer/crossfire/api/v1/crossfire/validators | jq | grep $OPA --after-context=26)
 
 MONIKER=$(printf "$OUTPUT" | grep "moniker" | cut -c 19- | sed 's/"//g' | sed 's/,//g')
@@ -22,6 +38,10 @@ CP3=$(printf "$OUTPUT" | grep "commitCountPhase3" | cut -c 29- | sed 's/"//g' | 
 P1BK=$(printf "$OUTPUT" | grep "phase1BlockCount" | cut -c 28- | sed 's/"//g' | sed 's/,//g')
 P2BK=$(printf "$OUTPUT" | grep "phase2BlockCount" | cut -c 28- | sed 's/"//g' | sed 's/,//g')
 P3BK=$(printf "$OUTPUT" | grep "phase3BlockCount" | cut -c 28- | sed 's/"//g' | sed 's/,//g')
+
+RANK1=$(printf "$OUTPUT" | grep "rankPhase1n2CommitmentRank" | cut -c 36- | sed 's/"//g' | sed 's/,//g')
+RANK2=$(printf "$OUTPUT" | grep "rankPhase3CommitmentRank"  | cut -c 34- | sed 's/"//g' | sed 's/,//g')
+RANK3=$(printf "$OUTPUT" | grep "rankTxSentRank"  | cut -c 24- | sed 's/"//g' | sed 's/,//g')
 
 UP2=$(echo $CP2 / $P2BK | bc -l | cut -c 2-3)
 
@@ -53,12 +73,16 @@ if [[ $TASKUPGRADE = "Completed" ]]
  else
  printf "Task 4:\e[31m Incomplete\e[0m\n"
 fi
-printf "\nTotal transactions sent: $TOTALTX\e[0m\n\n"
+printf "\nNormal Phase Commit Rank:$RANK1 \n"
+printf "Attack Phase Commit Rank:$RANK2 \n"
+printf "Transaction Rank:$RANK3 \n"
+
+printf "\nTotal Transactions Sent: $TOTALTX\e[0m\n\n"
 if (($UP2 > 50))
  then
- printf "Uptime in phase 2:\e[32m $UP2%%\e[0m\n\n"
+ printf "Uptime in Phase 2:\e[32m $UP2%%\e[0m\n\n"
 else
- printf "Uptime in phase 2:\e[31m $UP2%%\e[0m\n\n"
+ printf "Uptime in Phase 2:\e[31m $UP2%%\e[0m\n\n"
 fi
 
 exit 0
